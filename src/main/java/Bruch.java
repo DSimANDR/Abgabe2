60bfd8bd2cf022959c7fa6d514d02dac5d98001b
Dimitrije <test@gmail.com>
2021-11-07 19:04:58 +0100

    Bruch multiplicate: Retournieren der neuen Bruch-Variable

--- src/main/java/Bruch.java
+++ src/main/java/Bruch.java
@@ -35,9 +35,10 @@ public class Bruch {
         *   => Selbige auch für denominator
         *  Retournieren Sie ihre neue Bruch-Variable
         * */
+        Bruch b = new Bruch(this.numerator * b2.getNumerator(),
+                this.denominator * b2.getDenominator());
 
-
-        return null;
+        return b;
     }
 
     public Bruch multiplicate(Bruch b2,  Bruch b3) {
